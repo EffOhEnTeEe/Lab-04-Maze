317fe476224ad34d034f909b9c531a9401d3061e
Fonte Clanton <effohentee@gmail.com>
2014-04-22 16:43:43 -0400

    removed A2D interrupt and associated defined parameters

--- main.c
+++ main.c
@@ -29,51 +29,23 @@
 #pragma config FPBDIV       = DIV_8	// Peripheral bus clock divider
 #pragma config FSOSCEN      = OFF	// Secondary   oscillator enable
 
- // define setup parameters for OpenADC10
-// 				Turn module on | ouput in integer | trigger mode timer | enable autosample
-#define PARAM1  ADC_MODULE_ON | ADC_FORMAT_INTG32 | ADC_CLK_TMR | ADC_AUTO_SAMPLING_ON
-
-// define setup parameters for OpenADC10
-// 				ADC ref external    | disable offset test    | disable scan mode | perform 1 samples | use alternate mode
-#define PARAM2  ADC_VREF_AVDD_AVSS | ADC_SCAN_OFF | ADC_SAMPLES_PER_INT_1 |  ADC_ALT_INPUT_OFF
-
-// define setup parameters for OpenADC10
-//				  use ADC internal clock | peripheral bus clock
-#define PARAM3  ADC_CONV_CLK_Tcy2 | ADC_CONV_CLK_PB
-
-
-// define setup parameters for OpenADC10
-//               set AN0 as analog inputs
-#define PARAM4	ENABLE_AN2_ANA
-
-// define setup parameters for OpenADC10
-// do not assign channels to scan
-#define PARAM5	SKIP_SCAN_ALL
+ #define PERIPHERAL_CLOCK 10000000
 
 enum states { init } state;
 
 /*****************************************
 Global Variables
 *****************************************/
-uint32_t sec1000;   // Ms timer counter
+uint32_t sec1000;   // Millisecond timer counter
 uint32_t timer_a;   // Keeps tracks of timestamps
-uint32_t a2dRead;
 
 /*****************************************
  Interrupt Handlers
 *****************************************/
-void __ISR(_ADC_VECTOR, ipl1) ADCInterruptHandler(void) {
-    if( INTGetFlag(INT_AD1)) {
-        a2dRead = ReadADC10(0); // read the result of channel 0 conversion from the idle buffer
-        //channel1 = ReadADC10(offset + 1); // read the result of channel 1 conversion from the idle buffer
-        INTClearFlag(INT_AD1);
-    }
-}
-
-void __ISR(_TIMER_3_VECTOR, ipl2) TimerInterruptHandler(void) {
-    if( INT_SOURCE_TIMER(3)) {
-        sec1000 += 10;
-        INTClearFlag(INT_T3);
+void __ISR(_TIMER_2_VECTOR, ipl2) TimerInterruptHandler(void) {
+    if( INT_SOURCE_TIMER(2) ) {		// Verify source of interrupt
+        sec1000++;					// Increment timer counter
+        INTClearFlag(INT_T2);		// Acknowledge Interrupt
     }
 }
 
@@ -81,24 +53,16 @@ void main() {
     DelayInit();
     OledInit();
 
-    OpenTimer3(T3_ON            |
-           T3_IDLE_CON      |
-           T3_SOURCE_INT    |
-           T3_PS_1_16       |
-           T3_GATE_OFF,
-           6250); // trigger every 10 ms = 100 Hz
+    OpenTimer2(T2_ON            |
+	           T2_IDLE_CON      |
+	           T2_SOURCE_INT    |
+	           T2_PS_1_16       |
+	           T2_GATE_OFF,
+	           625); // trigger every 1 ms = 1 kHz [ 10MHz/16/625 = 1000 ]
 
-    SetChanADC10(ADC_CH0_NEG_SAMPLEA_NVREF | ADC_CH0_POS_SAMPLEA_AN2);
-    OpenADC10(PARAM1, PARAM2, PARAM3, PARAM4, PARAM5);
-
-    EnableADC10();
-
-    INTSetVectorPriority(INT_ADC_VECTOR,INT_PRIORITY_LEVEL_1);
-    INTClearFlag(INT_AD1);
-    INTEnable(INT_AD1, INT_ENABLED);
-    INTSetVectorPriority(INT_TIMER_3_VECTOR, INT_PRIORITY_LEVEL_2);
-    INTClearFlag(INT_INT3);
-    INTEnable(INT_T3, INT_ENABLED);
+    INTSetVectorPriority(INT_TIMER_2_VECTOR, INT_PRIORITY_LEVEL_1);
+    INTClearFlag(INT_T2);
+    INTEnable(INT_T2, INT_ENABLED);
     INTConfigureSystem(INT_SYSTEM_CONFIG_MULT_VECTOR);
     INTEnableInterrupts();
 
